27152a3d09aefed2238d347aa6954219236fb939
mahmoudh3sham <mhesham@sarmady.net>
2024-01-17 16:47:50 +0200

    add logger interceptor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
 
     //ViewModel
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
--- app/src/main/java/com/example/mealz/di/NetworkModule.kt
+++ app/src/main/java/com/example/mealz/di/NetworkModule.kt
@@ -1,11 +1,13 @@
 package com.example.mealz.di
 
 import com.example.data.remote.ApiService
+import com.example.mealz.utils.ApiLogger
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -21,6 +23,8 @@ object NetworkModule {
         return OkHttpClient.Builder()
             .connectTimeout(20, TimeUnit.SECONDS)
             .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor(ApiLogger()).setLevel(HttpLoggingInterceptor.Level.BODY))
             .build()
     }
 
